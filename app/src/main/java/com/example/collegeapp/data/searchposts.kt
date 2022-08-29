afca56a53de5c867c7ff42fa4ab9be7c275b2544
mahdi tavakoli~ <mahditavakoli7878@gmail.com>
2022-08-30 01:59:05 +0430

    Recyclerview :   for posts chip in search screen
    set windowSoftInputMode :
      to adjustPan

---
+++ app/src/main/java/com/example/collegeapp/data/searchposts.kt
@@ -0,0 +1,126 @@
+package com.example.collegeapp.data
+
+import android.content.res.Resources
+import com.example.collegeapp.Entities.ArticleEntity
+import com.example.collegeapp.R
+
+fun searchPostLists(): List<ArticleEntity> {
+    return listOf(
+        ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),
+        ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),  ArticleEntity(
+            title = "اسنپدراگون ۸ نسل یک پلاس احتمالاً ۲۰ درصد سریع است.".repeat(3),
+            time = "۲۱ فروردین ۱۴۰۱",
+            tag = "تکنولوژی",
+            image = R.drawable.background_image,
+            writer ="مهدی توکلی",
+        ),
+        )
+}
--- app/src/main/java/com/example/collegeapp/search/SearchFragment.kt
+++ app/src/main/java/com/example/collegeapp/search/SearchFragment.kt
@@ -2,11 +2,13 @@ package com.example.collegeapp.search
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.collegeapp.FragmentNavigationMethod
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.collegeapp.adapters.SearchPostAdapter
+import com.example.collegeapp.Entities.ArticleEntity
 import com.example.collegeapp.R
+import com.example.collegeapp.data.searchPostLists
 import com.google.android.material.chip.ChipGroup
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
@@ -18,10 +20,22 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         val checkedChipId = chipGroup.checkedChipId
         val checkedChipIds = chipGroup.checkedChipIds
 
+        chipGroup.setOnCheckedChangeListener { group, checkedId -> }
 
-        chipGroup.setOnCheckedChangeListener { group, checkedId ->
+        val searchPostAdapter = SearchPostAdapter()
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_postssearch_searchfragment)
+        recyclerView.adapter = searchPostAdapter
 
-        }
+        searchPostAdapter.submitList(searchPostLists())
+
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val data = ArrayList<ArticleEntity>()
+
+        val adapter = SearchPostAdapter()
+
+
 //        view.findViewById<Button>(R.id.btn_post_frSearch).setOnClickListener {
 //            FragmentNavigationMethod.navigate(
 //                action = R.id.action_searchFragment_to_childPostSearchFragment,
