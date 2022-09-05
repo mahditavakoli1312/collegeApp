cfcf83f5d24bc61d0ab144615b26fa0d3873f798
mahdi tavakoli~ <mahditavakoli7878@gmail.com>
2022-09-05 11:25:34 +0430

    Change FragmentNavigationMethos functions  to extensions functions

--- app/src/main/java/com/example/collegeapp/FragmentNavigationMethod.kt
+++ app/src/main/java/com/example/collegeapp/FragmentNavigationMethod.kt
@@ -2,17 +2,39 @@ package com.example.collegeapp
 
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+
+fun Navigation.easyNavigate(action: Int, navController: NavController) {
+    try {
+        navController.navigate(action)
+    } catch (e: IllegalArgumentException) {
+        e.printStackTrace()
+    }
+}
+
+fun Navigation.easyNavigateWithPopUp(
+    action: Int,
+    navController: NavController,
+    popUpId: Int,
+    inclusive: Boolean
+) {
+    val options = NavOptions.Builder().setPopUpTo(popUpId, inclusive).build()
+    try {
+        navController.navigate(action, null, options)
+    } catch (e: IllegalArgumentException) {
+        e.printStackTrace()
+    }
+}
 
 class FragmentNavigationMethod {
 
     companion object {
 
         fun navigate(action: Int, navController: NavController) {
-            try {
-                navController.navigate(action)
-            } catch (e: IllegalArgumentException) {
-                e.printStackTrace()
-            }
+            Navigation.easyNavigate(
+                action = action,
+                navController = navController
+            )
 
         }
 
@@ -22,12 +44,12 @@ class FragmentNavigationMethod {
             popUpId: Int,
             inclusive: Boolean
         ) {
-            val options = NavOptions.Builder().setPopUpTo(popUpId, inclusive).build()
-            try {
-                navController.navigate(action, null, options)
-            } catch (e: IllegalArgumentException) {
-                e.printStackTrace()
-            }
+            Navigation.easyNavigateWithPopUp(
+                action = action,
+                navController = navController,
+                popUpId = popUpId,
+                inclusive = inclusive
+            )
         }
 
     }
